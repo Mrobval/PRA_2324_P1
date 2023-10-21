de1390c1efec5dc6c6dd4f0ddb7d0747fca1898a
koku <mrobe2503@gmail.com>
2023-10-21 18:46:04 +0200

    Añadido Makefile y código de test de la clase ListArray

--- ListArray.h
+++ ListArray.h
@@ -2,6 +2,8 @@
 #include "List.h"
 #include <stdexcept>
 
+#define NUM_INICIAL 10
+
 template<typename T>
 
 class ListArray : public List<T> {
@@ -19,9 +21,6 @@ class ListArray : public List<T> {
 			T* new_arr = new T[new_size];
 
 			if (new_size <= size()){
-				
-				T* new_arr = new T[new_size];
-				
 				for (int i=0; i < new_size; i++){
 					new_arr[i] = arr[i];
 				}
@@ -40,18 +39,29 @@ class ListArray : public List<T> {
                 ListArray(){
 			max = MINISIZE;
 			n = 0;
-			arr = new T[max];
+			arr = new T[NUM_INICIAL];
 		};
                 ~ListArray(){
 			delete[] arr;
 		};
-                T operator[] (int pos){
+                T operator[] (int pos) const{
 			if (pos < 0 || pos > size()){
 				throw std::out_of_range("Posición no válida");
 			}
 			return arr[pos];
 		};
-                friend std::ostream& operator<<(std::ostream &out, const ListArray<T> &list);
+                friend std::ostream& operator<<(std::ostream &out, const ListArray<T> &list){
+			if(list.size() == 0){
+				out << "List => [";
+			} else{
+				out << "List => [" << std::endl;
+				for (int i = 0; i < list.size(); i++){
+					out << " "<< list[i] << std::endl;
+				}
+			}
+			out << "]"; 
+			return out;
+		};
                 void insert(int pos, T e) override{
 			if (pos < 0 || pos > size()){
 				throw std::out_of_range("Posición no válida");
@@ -59,31 +69,34 @@ class ListArray : public List<T> {
 			
 			if( size() == max){
 				//Implementar redimension del array cuando size = max
-				resize( max + 10);
+				resize(max+5);
 			}
 			for (int i = size(); i>pos; i--){
 				arr[i] = arr[i-1];
 			}
 			arr[pos] = e;
+
 			n++;
 		};
-                void apend(T e) override{
+                void append(T e) override{
 			insert(size(), e);
 		};
-                void prepend(T e) override{
+                void prepend(T e)  override{
 			insert(0, e);
 		};
                 T remove(int pos) override{
 			if (pos < 0 || pos > size()-1){
                                 throw std::out_of_range("Posición no válida");
-                        }
-			if(size() <= max - 10){
-				resize(max - 5);
 			}
 			T ElementoEliminado = arr[pos];
 			for (int i = pos; i < size()-1; i++){
                                	arr[i] = arr[i+1];
                        	}
+			n--;
+
+			if (size() <= max - 10){
+				resize(max-5);
+			}
                         return ElementoEliminado;
 		};
                 T get(int pos) override{
@@ -100,11 +113,11 @@ class ListArray : public List<T> {
 			}
 			return -1;
 		};
-                bool empety() override{
+                bool empty() const override{
 			//De esta forma, si size = cero devuelve true y si es mayor a cero devuelve false
 			return size() == 0; 
 		};
-                int size() override{
+                int size() const override{
 			return n;
 		};
 
